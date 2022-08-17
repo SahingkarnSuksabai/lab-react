f8eb10614e697499bc2413b0df2d8cfaa91b0f4f
SahingkarnSuksabai <6310110471@psu.ac.th>
2022-08-18 03:56:11 +0700

    Game Logic

--- src/WorldCard.js
+++ src/WorldCard.js
@@ -1,17 +1,52 @@
-import React  from 'react';
+import React, { useState }  from 'react';
+import _, { at, attempt, set } from 'lodash';
+
 import CharacterCard from './CharacterCard';
 
+const prepareStateFromWord = given_word => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
+    }
+
+}
+
 export default function WordCard(props){
+    const [state, setState] = useState(prepareStateFromWord(props.value))
+
     const activateHandler = c => {
         console.log(`${c} has been activated.`)
+
+        let guess = state.guess + c
+        setState({...state, guess})
+
+        if(guess.length == state.word.length){
+            if(guess == state.word){
+                console.log('yeah!')
+                setState({...state, completed: true})
+
+        }else{
+            console.log('reset, next attempt')
+            setState({...state, guess: '', attempt: state.attempt + 1})
+        }
+        }
     }
     return (
         <div>
             {
-                Array.from(props.value).map((c, i) =>
-                   <CharacterCard value={c} key={i} activateHandler={activateHandler}/>
+                state.chars.map((c, i) =>
+                   <CharacterCard value={c} key={i} activateHandler={activateHandler} attempt={state.attempt}/>
                 )
             }
         </div>
     )
-}
+    }
+
+
+
+    
